987cfa0015035eb72b7e658f50bf6c85d92e2442
Jacob Daddario <jake.daddario@gmail.com>
2020-08-26 12:55:02 -0500

    Added basic styling to the CTA and made some responsive adjustments

---
+++ src/CallToAction.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CallToAction = () => (
+  <div className="flex flex-col items-center max-w-2xl mx-auto px-4 pt-12 xl:pt-20 2xl:pt-32 2xl:ml-32">
+    <h2 className="text-center text-gray-500 text-xl tracking-wide font-thin">
+      How can we help you?
+    </h2>
+    <div className="flex flex-col items-center w-full h-64 max-w-md mt-4 px-4 pt-8 bg-white rounded shadow-md 2xl:max-w-xl"></div>
+  </div>
+);
+
+export default CallToAction;
--- src/HeroText.js
+++ src/HeroText.js
@@ -2,9 +2,9 @@ import React from "react";
 import secLogoWhite from "./assets/images/sec-logo-white.png";
 
 const HeroText = () => (
-  <div className="flex flex-col items-center max-w-2xl mx-auto px-4 pt-6 xl:pt-16 2xl:pt-20 2xl:mr-40">
+  <div className="flex flex-col items-center max-w-2xl mx-auto px-4 pt-6 xl:pt-16 2xl:pt-20 2xl:mr-32">
     <img src={secLogoWhite} alt="white SEC logo" className="h-48 2xl:h-56" />
-    <h2 className="text-center text-white text-2xl leading-snug tracking-tight font-semibold xl:text-3xl xl:leading-tight 2xl:text-3xl">
+    <h1 className="text-center text-white text-2xl leading-snug tracking-tight font-semibold xl:text-3xl xl:leading-tight 2xl:text-3xl">
       Welcome to the SEC,
       <br />
       <span className="text-maroon-700 px-2 bg-yellow-200 bg-opacity-50 rounded">
@@ -12,7 +12,7 @@ const HeroText = () => (
       </span>
       <br />
       at Texas A&amp;M!
-    </h2>
+    </h1>
   </div>
 );
 
